c4a28ea6abd4cb971a10ef4d19331a58c207df7d
chrissmithq9 <chris.smith@q9.com>
2019-06-11 09:40:28 -0400

    Changing proxy URL to use first proxy with external_ipam feature enabled

--- lib/foreman_ipam/phpipam_client.rb
+++ lib/foreman_ipam/phpipam_client.rb
@@ -2,7 +2,8 @@
 module ForemanIpam
   class PhpipamClient
     def self.proxy
-      ProxyAPI::ExternalIpam.new({:url => SmartProxy.first[:url]})
+      external_ipam_proxy = SmartProxy.with_features('external_ipam').first
+      ProxyAPI::ExternalIpam.new({:url => external_ipam_proxy[:url]})
     end
   end
 end
